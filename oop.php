51c451234465f7053522823e9ed5b5ccec109c42
Faridul Islam <73481902+bddevfarid@users.noreply.github.com>
2023-05-31 12:44:34 +0600

    Update oop.php

--- oop.php
+++ oop.php
@@ -1,1 +1,25 @@
+PHP What is OOP?
+OOP stands for Object-Oriented Programming.
 
+Objects - 
+1. Properties - ki ki ace 
+2. Methods - ja ja korte pare
+
+class Man{
+  public $name;
+  public $age;
+
+  //Methods
+  public function name(){
+     return "My name is " . $this->name;
+  }
+
+  public function age(){
+     return "I am " . $this->name . " years old";
+  }
+
+}
+
+$man = new Man();
+echo $man->name("Farid");
+echo $man->age(27);
